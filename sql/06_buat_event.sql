4e81efea66aa7643018f3db0b5a91bcb69a05aa9
arifdwifebrianto <arifdwfe@gmail.com>
2025-12-12 07:33:15 +0700

    Tambah code Demonstrasi

--- sql/06_buat_event.sql
+++ sql/06_buat_event.sql
@@ -10,3 +10,21 @@ BEGIN
     WHERE status_pesanan = 'Pending' 
     AND tanggal_pesanan < (NOW() - INTERVAL 1 DAY);
 END
+
+/*
+-- Buat Demonstrasi
+-- Hapus event lama dulu
+DROP EVENT IF EXISTS ev_auto_cancel_pesanan_kedaluwarsa;
+
+-- Buat event baru
+CREATE EVENT ev_auto_cancel_pesanan_kedaluwarsa
+ON SCHEDULE EVERY 30 SECOND -- (A) Robot mengecek tiap 30 detik
+STARTS CURRENT_TIMESTAMP
+DO
+BEGIN
+    UPDATE pesanan
+    SET status_pesanan = 'Dibatalkan'
+    WHERE status_pesanan = 'Pending' 
+    AND tanggal_pesanan < (NOW() - INTERVAL 1 MINUTE); -- (B) Kadaluarsa setelah 1 menit
+END
+*/
